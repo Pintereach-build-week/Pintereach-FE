ee71102ca468f6b79b9687dafe2a0ed429849282
Lauren Emick <laurenemick6@gmail.com>
2020-06-23 12:59:36 -0700

    dummy data added, and articlelist and articlecard complete
    Co-authored-by: Hailey <haileyhansard@gmail.com>
    Co-authored-by: Lauren <laurenemick6@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-<<<<<<< HEAD
-import Login from './components/auth/Login'
-=======
+import React, { useState } from 'react';
 import { Navbar } from './components/navbar/Navbar';
 import { ArticleList } from './components/articles/ArticleList';
 import { Catergory } from './components/category/Catergory';
-
->>>>>>> c76705e1d251c0536ee11ecc414c8171af845f63
+import Login from './components/auth/Login'
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Login />
-=======
-
       <Navbar/>
+      <Login />
       <Catergory />
-      <ArticleList/>
-
->>>>>>> c76705e1d251c0536ee11ecc414c8171af845f63
+      <ArticleList />
     </div>
   );
 }
