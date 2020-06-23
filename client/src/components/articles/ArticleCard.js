ac916e00f84541bc8160b8100091c2a53b5e8126
JM <manzur-1990@hotmail.com>
2020-06-23 11:15:12 -0500

    Added comment input

--- client/src/components/articles/ArticleCard.js
+++ client/src/components/articles/ArticleCard.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Comments } from '../comments/Comments'
 
 export const ArticleCard = () => {
     return (
         <div>
             Article 
+            <Comments />
         </div>
     )
 }
