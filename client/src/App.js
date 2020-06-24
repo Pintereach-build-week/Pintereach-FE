105756bdbbd8d81a0f7b68caa2c8344def3a26f1
haileyhansard <haileyhansard@gmail.com>
2020-06-23 17:41:29 -0700

    Push again without register component in the app file

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Navbar } from './components/navbar/Navbar';
 import { ArticleList } from './components/articles/ArticleList';
 import { Catergory } from './components/category/Catergory';
-import Register from './components/auth/Register'
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Navbar/>
       <Catergory />
       <ArticleList />
-      <Register />
     </div>
   );
 }
