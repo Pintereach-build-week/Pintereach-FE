63770e43545b9588f495af781a38697a7cdeece6
Lauren Emick <laurenemick6@gmail.com>
2020-06-25 09:36:53 -0700

    added links to Navbar file
    Co-authored-by: Hailey <haileyhansard@gmail.com>
    Co-authored-by: Lauren <laurenemick6@gmail.com>

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
     return (
         <div>
             <nav>
                 <h1>Pintreach</h1>
-                <ul>
-                    <li>Reading List</li>
-                    <li>My Profile</li>
-                    <li>logout</li>
-                </ul>
+                <Link to='/'>Home</Link>
+                <Link to='/saved-list'>Saved List</Link>
+                <Link to='/register'>Register</Link>
+                <Link to='/login'>Login In</Link>
+                <Link to='/logout' >Log Out</Link>
             </nav>
         </div>
     )
